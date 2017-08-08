34c750c4a504b3be33b5a2a028010a2a466ec86b
Mario Dian <mariodian@gmail.com>
2017-08-08 18:44:14 +0800

    Add lock-in switch

--- sync.php
+++ sync.php
@@ -9,6 +9,7 @@ $data = file_get_contents('http://api.qbit.ninja/versionstats');
 $obj = json_decode($data);
 $count = NULL;
 $percentage = NULL;
+$sql_locked_in = '';
 
 if ($obj->sincePeriodStart) {
   foreach ($obj->sincePeriodStart->stats as $item) {
@@ -20,7 +21,12 @@ if ($obj->sincePeriodStart) {
   }
 
   if ($count !== NULL && $percentage !== NULL) {
-    $sql = "UPDATE data SET count=$count, percentage=$percentage, last_updated='" . date("Y-m-d H:i:s") . "'";
+    // SegWit is locked in on 1916 blocks within a period
+    if ($count >= 1916) {
+        $sql_locked_in = ' , locked_in = 1';
+    }
+    
+    $sql = "UPDATE data SET count=$count, percentage=$percentage, last_updated='" . date("Y-m-d H:i:s") . "'$sql_locked_in";
 
     if ($conn->query($sql) === TRUE) {
       echo "Record updated successfully";
